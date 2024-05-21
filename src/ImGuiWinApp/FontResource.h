ed33eeef64bb4180b94c9c14cc984d2986d52089
keqing996 <epigrf@outlook.com>
2024-05-21 10:24:39 +0800

    Organize code.

--- src/ImGuiWinApp/FontResource.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef EMBEDDED_TTF
-#   define RES_TTF_REGULAR 9901
-#   define RES_TTF_BOLD 9902
-#   define RES_TTF_ITALIC 9903
-#   define RES_TTF_BOLD_ITALIC 9904
-#endif
-// a newline must be here
