8abdc1a9ff016d7938ed94e78f5ee112040d801f
Noemie <epigrf@outlook.com>
2024-10-13 13:53:00 +0800

    Update

--- example/Main.cpp
+++ example/Main.cpp
@@ -1,8 +1,19 @@
-#include "ImGuiWinApp/Application.h"
+#include "ImApp/ImGuiWinApp.h"
 
 int main()
 {
+    NativeWindow::Window window(800, 600, "TestOpenGL");
+    while (true)
+    {
+        window.EventLoop();
 
+        if (std::ranges::any_of(window.PopAllEvent(), [](const NativeWindow::WindowEvent& event) -> bool { return event.type == NativeWindow::WindowEvent::Type::Close; }))
+            break;
+
+
+
+        window.SwapBuffer();
+    }
 
     return 0;
 }
