2abadb0459cc6bd97c086d9926642faeca438f17
keqing996 <epigrf@outlook.com>
2024-10-14 18:21:22 +0800

    vulkan

---
+++ src/ImGuiBackend/ImGuiBackendVulkan.cpp
@@ -0,0 +1,37 @@
+#include "ImGuiBackendVulkan.h"
+#include <backends/imgui_impl_vulkan.h>
+
+namespace IMWinApp
+{
+    void ImGuiBackendVulkan::SetupDevice()
+    {
+    }
+
+    void ImGuiBackendVulkan::SetupImGui()
+    {
+    }
+
+    void ImGuiBackendVulkan::ClearImGui()
+    {
+    }
+
+    void ImGuiBackendVulkan::ClearDevice()
+    {
+    }
+
+    void ImGuiBackendVulkan::NewFrame()
+    {
+    }
+
+    void ImGuiBackendVulkan::Draw()
+    {
+    }
+
+    void ImGuiBackendVulkan::SetClearColor(const Utility::Color& color)
+    {
+    }
+
+    void ImGuiBackendVulkan::SwapBuffer()
+    {
+    }
+}
